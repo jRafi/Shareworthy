edbc5b133a78cc20d3b1689b0b9ea82ab5feff2b
jRafi <jonas.rafi@hotmail.com>
2017-01-18 09:36:05 +0100

    Updated comments

--- tabbly.R
+++ tabbly.R
@@ -1,13 +1,15 @@
 
+# The problem: tabulating all columns in a df returns unneccessary tables (like id:s), while
+# tabulating each relevant column separately involves unneccessary typing.
 
-# Sapply table on all the factor-columns in a dataframe
-# Equivalent to mapply table only on factor-columns
+# The solution: tabbly is a function that checks which columns in data.frame x are factors, and then
+# sapply:s the table function on those columns. Equivalent to mapply table only on factor-columns.
 
 tabbly<-function(x) {
         v<-vector()
         for(i in names(x)) {
                 if(is.factor(x[,(i)])) {
-                v<-c(v, (i))        
+                        v<-c(v, (i))        
                 }
         }
         sapply(x[,v], table)
