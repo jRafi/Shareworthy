3196e9a6c15feb4c291e3e192e5067f826b31a55
jRafi <jonas.rafi@hotmail.com>
2017-01-19 11:04:06 +0100

    Minor edit

--- irriTable.R
+++ irriTable.R
@@ -1,16 +1,15 @@
 
 ### irriTable
 
-# The problem: Output a frequency table from a dataframe with survey scores (or other factor)
-# and an index variable.
+# The problem: Need to output a frequency table from a dataframe with survey scores (or other 
+# factor) and an index variable.
 
 # The solution: subset the dataframe so it only contains the desired table row and column, tabulate
-# it, convert it to a dataframe and keep the table rows as a variable. Stored as "tabledf".
+# it, convert it to a dataframe and keep the index as a variable.
 
 irriTable<-function(x, tableRow="", tableCol="") {
         df<-as.data.frame.matrix(table(subset(x[,c(tableRow,tableCol)])))
-        df<-as.data.frame(cbind(index=rownames(df), df), row.names = NULL)
+        df<-as.data.frame(cbind(index=rownames(df), df))
         row.names(df)<-NULL
-        tabledf<-df
         df
 }      
