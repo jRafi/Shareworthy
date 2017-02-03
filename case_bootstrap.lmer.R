cb5432d743b664f1c40615d9abb9a0d21db62298
Jonas <jRafi@users.noreply.github.com>
2017-02-03 16:08:42 +0100

    Minor cleanups + highlighted potential nlme-bug

--- case_bootstrap.lmer.R
+++ case_bootstrap.lmer.R
@@ -1,36 +1,50 @@
 
-
-library(lme4)
-library(nlme)
 library(boot)
 library(lmeresampler)
 
-
+# Generate some poisson-distributed hierarchical data, along with two factors and a time variable.
 set.seed(123)
 x<-data.frame("id"=rep(1:100, each=5),
               "f1"=sample(0:1,250, replace=T),
               "f2"=sample(0:1,250, replace=T),
               "time"=rep(1:5,100),
               "y"=rpois(500,1)
-              )
+)
 
 hist(x$y) # Display the distribution
 
-# Model using nlme
-m1<-lme(y ~ time*f1*f2, data=x, random= ~ time|id, na.action = na.omit)
-
-# Model using lme4
-m2<-lmer(y ~ time*f1*f2+(time|id), x, na.action=na.omit)
 
 
-### Prepare the bootstrap
+# Using nlme::lme
+library(nlme)
+myModel<-lme(y ~ time*f1*f2, data=x, random= ~ time|id, na.action = na.omit)
 
-# Create function that is needed in the case_bootsrap function to extract statistics
-myfn<-function(x=m2){fixef(x)}
+# Using lme4::lmer
+library(lme4)
+myModel<-lmer(y ~ time*f1*f2+(time|id), x, na.action=na.omit)
 
-# Bootstrap and store results in m2boot
-m2boot<-case_bootstrap(model=m2, B=100, fn=myfn, resample = c(TRUE,FALSE))
-summary(m2boot)
+# Create function that extracts fixed effects
+myfn<-function(x=myModel){fixef(x)}
+
+
+#========================
+### Using case_bootstrap()
+#========================
+
+# Bootstrap and store results in myBootstrapData
+# Error: this only seem to work with lmer, not lme.
+myBootstrapData<-case_bootstrap(model=myModel, B=100, fn=myfn, resample = c(TRUE,FALSE))
+summary(myBootstrapData)
+
+
+#====================
+### Using bootstrap()
+#====================
+
+myBootstrapData<-bootstrap(model=myModel, fn=myfn, type="case", B=100, resample=c(TRUE, FALSE))
+summary(myBootstrapData)
+
+
 
 
 ### Extract bootstrap parameters
@@ -40,4 +54,3 @@ boot.ci(m2boot, index = 1,type=c("norm", "basic", "perc"))
 
 # Plot a specific statistic
 plot(m2boot, index = 1)
-
